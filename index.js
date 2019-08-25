f62eda7c0790891ea7ec4d3ec8641f0aaaa8c09c
Tamir Korem <tamirkorem@gmail.com>
2019-08-25 12:47:25 +0300

    25 08 v01
    25 08 v01

--- index.js
+++ index.js
@@ -1,12 +1,33 @@
+/** 
 var mime = require('mime')
 var express = require('express')
 var app = express()
  
- 
- 
- mime.lookup();
+  
+mime.Mime();  
+mime.lookup("aaaaaa",null);
 app.get('/', function (req, res) {
   res.send('Hello World')
 })
  
+app.listen(3000)
+
+*/
+
+
+
+var express = require('express')
+var app = express()
+
+function g(){ 
+var mime = require('mime') 
+mime.Mime();  
+mime.lookup("aaaaaa",null); 
+}
+g();
+
+app.get('/', function (req, res){ 
+res.send('Hello World') 
+})
+
 app.listen(3000)
